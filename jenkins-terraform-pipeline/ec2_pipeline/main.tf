1f18d34e810df5c28f90bcb41a4fab95b6fe6951
Anis Sayyad <anis.sayyad@ad.infosys.com>
2020-09-23 15:12:02 +0530

    added updated

--- jenkins-terraform-pipeline/ec2_pipeline/main.tf
+++ jenkins-terraform-pipeline/ec2_pipeline/main.tf
@@ -3,7 +3,12 @@ provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
 }
-
 terraform {
+  backend "s3" {
+    bucket = "blockchain-project1"
+    key    = "network/terraform.tfstate"
+    region = "us-east-1"
+  }
   required_version = "~> 0.12"
 }
+
